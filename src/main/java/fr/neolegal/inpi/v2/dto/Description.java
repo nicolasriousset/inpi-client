11498107a06c261b3068b0d5bfb21239cfdd95d3
sylvestre <sylvestre@pinget.fr>
2024-07-02 15:39:41 +0200

    clean import

--- src/main/java/fr/neolegal/inpi/v2/dto/Description.java
+++ src/main/java/fr/neolegal/inpi/v2/dto/Description.java
@@ -1,10 +1,9 @@
 package fr.neolegal.inpi.v2.dto;
 
-import java.time.Instant;
-import java.time.LocalDate;
-
 import lombok.Getter;
 import lombok.Setter;
+
+import java.time.LocalDate;
 
 @Getter
 @Setter
